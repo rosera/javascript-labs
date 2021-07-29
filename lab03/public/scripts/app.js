02634da77c484c84172c2808e13998b56e12b1d2
rosera <askrichardrose@gmail.com>
2021-07-29 17:41:19 +0100

    Fix: remove redundant code

--- lab03/public/scripts/app.js
+++ lab03/public/scripts/app.js
@@ -1,11 +1,3 @@
 // Use the values from the HTML template
-const inputPlay  = document.querySelector('input');
-const paraLink   = document.querySelector('#cloud-hero-link');
-const paraCode   = document.querySelector('#cloud-hero-code');
 
-// Watch for submit and update the form
-inputPlay.addEventListener('click', (event) => {
-
-  let x = document.getElementById("hero-header");
-  x.style.display = "block";
-})
+// Add your cool code here!
